db52147745908ed5e44b45f58795df7a67b62606
Niccolò Ceravolo <niccoloceravolo@MacBook-Pro-di-Niccolo.local>
2025-11-06 13:31:17 +0100

    grid

---
+++ scripts/generate-gallery.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const imagesDir = path.join(__dirname, '../public/images');
+const outFile = path.join(__dirname, '../public/gallery.js');
+
+const files = fs.readdirSync(imagesDir)
+  .filter(f => /\.(jpg|jpeg)$/i.test(f));
+
+const output = `window.galleryImages = ${JSON.stringify(files.map(f => 'images/' + f), null, 2)};`;
+fs.writeFileSync(outFile, output);
+
+console.log('🖼 gallery.js generato!');

392ea902666d4e790c68c31e6a2aa6efc6dbfe1d
Niccolò Ceravolo <niccoloceravolo@Host-039.homenet.telecomitalia.it>
2025-11-09 09:57:38 +0100

    foto

--- gallery.js
+++
@@ -1,9 +0,0 @@
-window.galleryImages = [
-  "public/images/2022.jpg",
-  "public/images/Brugada 1.jpg",
-  "public/images/Brugada 2.jpg",
-  "public/images/EDIZIONE2023.jpg",
-  "public/images/EDIZIONE2024.jpg",
-  "public/images/diretta.jpg",
-  "public/images/ed3.jpg"
-];
